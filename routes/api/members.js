253c0cfe62a82f14c4bcf2d70691a5a9da2c0e42
binary-greens <phyartfoot@gmail.com>
2020-11-10 19:18:33 +0530

    delete added

--- routes/api/members.js
+++ routes/api/members.js
@@ -67,5 +67,20 @@ router.put('/:id', (req, res) => {
     }
 })
 
+// Delete member
+router.delete('/:id', (req, res) => {
+    const found = members.some(member => {
+        // console.log("url:", parseInt(req.params.id), "member.id:", member.id);
+        return member.id === parseInt(req.params.id);
+    })
+    console.log(found);
+    if (found) {
+        res.json({msg: "Member deleted", members: members.filter(member => member.id !== parseInt(req.params.id))});
+    } else {
+        res.status(400).json({
+            msg: `Member not found with id ${req.params.id}`
+        })
+    }
+})
 
 module.exports = router;
