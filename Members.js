21af5872a6e1584f74c6bbcbcd5f7a44f828319d
binary-greens <phyartfoot@gmail.com>
2020-11-10 11:44:17 +0530

    initial

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'Arpan Maity',
+        email: 'arpan@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Arup Kumar',
+        email: 'akumar@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Aparna Maity',
+        email: 'aparna@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 4,
+        name: 'Aruparna Maity',
+        email: 'ario@gmail.com',
+        status: 'iactive'
+    }
+]
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const { send } = require('process');
+const members = require('./Members');
+
+const app = express();
+
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    console.log("----------------");
+    next();
+}
+
+// Init middleware
+app.use(logger)
+
+// Get all members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+})
+
+// ---------------------------------------------------------------------
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+// ---------------------------------------------------------------------
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
